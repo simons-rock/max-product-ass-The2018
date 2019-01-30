e9f6ed89303f9bc8a237d47568a06a3c9fc67740
Tianxiao He <tianxiaohe@Tianxiaos-MacBook-Pro.local>
2019-01-29 20:34:22 -0500

    finished

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,33 @@
 #include "max_product.h"
 
-int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+void swap(int *a, int *b){
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
+
+int max_pairwise_product_fast(int *arr, int size){
+	int index = 0; 
+
+	for (int i = 1; i < size; i++){
+		if (arr[i]>arr[index]){
+			index = i;
+		}
+	}
+	swap(&arr[index], &arr[size-1]);
+
+	index = 0;
+	for (int i = 1; i < size - 1; i++){
+		if (arr[i]>arr[index]){
+			index = i;
+		}
+	}
+	swap(&arr[index], &arr[size-2]);
+
+	return arr[size-2]*arr[size-1];
+
+}
+
+
 
